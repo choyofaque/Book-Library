46a37134a1a46c4e44ecd45eb6feefad5101bffb
Manuel Streit <manuel.streit@icloud.com>
2022-02-15 14:58:43 +0100

    Initial Commit

---
+++ Book Library/Book_LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Book_LibraryApp.swift
+//  Book Library
+//
+//  Created by Manuel Streit on 15.02.22.
+//
+
+import SwiftUI
+
+@main
+struct Book_LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
